a826530de380375737811c5ac8f1283480137442
Martin Frost Lauridsen <mal021@edu.zealand.dk>
2024-11-28 12:43:29 +0100

    Boat class

--- BådLibrary/Boat.cs
+++ BådLibrary/Boat.cs
@@ -3,6 +3,12 @@
     public class Boat
     {
         public int Id { get; set; } 
+        public double RentalPrice { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public int RegNumber { get; set; }
+
         
     }
 }
