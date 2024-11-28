7a0ad64bdddb59992b025822841c763a6330478b
Martin Frost Lauridsen <mal021@edu.zealand.dk>
2024-11-28 12:50:30 +0100

    Boat class constructor

--- BådLibrary/Boat.cs
+++ BådLibrary/Boat.cs
@@ -8,7 +8,26 @@
         public string Model { get; set; }
         public string Name { get; set; }
         public int RegNumber { get; set; }
+        public int HorsePower { get; set; }
+        public int Knots { get; set; }
+        public double Height { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
 
-        
+        public Boat(int id, double rentalPrice, string type, string model, string name, int regNumber, int horsePower, int knots, double height, double length, double width)
+        {
+            Id = id;
+            RentalPrice = rentalPrice;
+            Type = type;
+            Model = model;
+            Name = name;
+            RegNumber = regNumber;
+            HorsePower = horsePower;
+            Knots = knots;
+            Height = height;
+            Length = length;
+            Width = width;
+        }
     }
+
 }
